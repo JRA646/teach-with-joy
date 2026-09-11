9f08af05b4b74e1e7a4a7b78f76a40d8359a4917
JRA646 <149916056+JRA646@users.noreply.github.com>
2026-09-12 03:50:04 +0800

    style: load Tailwind design system across the app

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './responsive.css'
 import './profile.css'
 import './admin.css'
 import './site-theme.css'
+import './tailwind.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
